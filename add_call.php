0437bdd046b24a3efbb4fc597e336ba2a52fd9dc
Sammed Badde <sammedbadde10@gmail.com>
2026-05-02 17:00:56 +0530

    Update add_call.php

--- add_call.php
+++ add_call.php
@@ -4,7 +4,6 @@ $enquiry_id = $_POST['enquiry_id'];
 $date = $_POST['call_date'];
 $status = $_POST['call_status'];
 $remarks = $_POST['remarks'];
-
 mysqli_query($conn, "INSERT INTO call_records 
 (enquiry_id, call_date, call_status, remarks)
 VALUES ('$enquiry_id','$date','$status','$remarks')");
